aeb685c7fdf3385a15f40fde805a1a3a284b8a8b
Ondřej Švarc <121796880+ondrejsvarc@users.noreply.github.com>
2023-05-18 11:36:59 +0200

    draw_tools

--- draw_tools.c
+++ draw_tools.c
@@ -1,13 +1,12 @@
-unsigned short *fb;
-fb  = (unsigned short *)malloc(320*480*2);
 
-void draw_pixel(int x, int y, unsigned short color) {
+
+void draw_pixel ( int x, int y, unsigned short color, unsigned short *fb ) {
   if (x>=0 && x<480 && y>=0 && y<320) {
     fb[x+480*y] = color;
   }
 }
 
-void drawTile ( int index, unsigned short color ) {
+void drawTile ( int index, unsigned short color, unsigned short *fb ) {
     int x = index % 24;
     int y = ( index - x ) / 16;
     int startX = x * 20;
@@ -21,8 +20,4 @@ void drawTile ( int index, unsigned short color ) {
         }
     }
 
-}
-
-for (ptr = 0; ptr < 480*320 ; ptr++) {
-    parlcd_write_data(parlcd_mem_base, fb[ptr]);
 }
---
+++ draw_tools.h
@@ -0,0 +1,5 @@
+
+
+
+void draw_pixel ( int x, int y, unsigned short color, unsigned short *fb );
+void drawTile ( int index, unsigned short color, unsigned short *fb );
