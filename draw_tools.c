8f6e90e3911a5edce6faa2fe3ff0a77ac8ad458f
Ondřej Švarc <121796880+ondrejsvarc@users.noreply.github.com>
2023-05-18 11:52:43 +0200

    draw_tools update

--- draw_tools.c
+++ draw_tools.c
@@ -1,5 +1,3 @@
-
-
 void draw_pixel ( int x, int y, unsigned short color, unsigned short *fb ) {
   if (x>=0 && x<480 && y>=0 && y<320) {
     fb[x+480*y] = color;
@@ -20,4 +18,19 @@ void drawTile ( int index, unsigned short color, unsigned short *fb ) {
         }
     }
 
+}
+
+void drawFruit ( int index, unsigned short *fb ) {
+    int x = index % 24;
+    int y = ( index - x ) / 16;
+    int startX = x * 20 +5 ;
+    int maxX = startX + 10;
+    int startY = y * 20 + 5;
+    int maxY = startY + 10;
+
+    for ( int i = startX; i < maxX; ++ i ) {
+        for ( int j = startY; j < maxY; ++j ) {
+            draw_pixel( i, j, 0xf800, fb );
+        }
+    }
 }
