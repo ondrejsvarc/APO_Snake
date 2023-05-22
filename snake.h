41b856048b58e8a70b0001171c1bbeb6a9d60be4
Michal Komanec <michal.komanec@czechitas.cz>
2023-05-22 18:34:05 +0200

    Snake header

---
+++ snake.h
@@ -0,0 +1,22 @@
+#ifndef SNAKE_H
+
+#include <stdbool.h>
+#ifndef MAP_ROWS
+#define MAP_ROWS 16
+#endif
+
+#ifndef  MAP_COLS
+#define MAP_COLS 24
+#endif
+
+typedef struct {
+    short heading[2];
+    short body[MAP_COLS*MAP_ROWS];
+    unsigned short length;
+} Snake;
+
+/*Function that takes snake struct and heading of the snake and moves the snake*/
+void move_snake(Snake *snake, short direction);
+
+void change_heading(Snake *snake, short heading_change);
+#endif
