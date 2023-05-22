a0e378c8b06dae3afad1fa574b6b5f4b4f5a7b03
Michal Komanec <michal.komanec@czechitas.cz>
2023-05-22 20:15:50 +0200

    Main.c impl

---
+++ main.c
@@ -0,0 +1,47 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "mzapo_parlcd.h"
+#include "mzapo_phys.h"
+#include "mzapo_regs.h"
+#include "draw_tools.h"
+#include "snake.h"
+#include "led_tools.h"
+#include "main.h"
+
+
+unsigned short *fb;
+
+int main(int argc, char *argv[]) {
+    FILE *menu_img_file = fopen("menu_img.ppm", 'rb');
+    if (menu_img_file == NULL) {
+        fprintf(stderr, "Error opening menu_img.ppm");
+        exit(-1);
+    }
+
+    unsigned char *menu_img_buffer = (unsigned char*) malloc(320*480*3);
+    if (menu_img_buffer == NULL) {
+        fprintf(stderr, "Couldn't allocate memory for menu image buffer!");
+        exit(-1);
+    }
+
+    fb = (unsigned short *)malloc(320*480*2);
+    if (fb == NULL) {
+        fprintf(stderr, "Couldn't allocate memory for frame buffer!");
+        exit(-1);
+    }
+    for (int i = 0; i < 4; i++) {
+        fscanf(menu_img_file, "%s\n");
+    }
+    
+    fread(menu_img_buffer, sizeof(unsigned char), 320*480*3, menu_img_file);
+    for (int i = 0; i < 320*480; i++) {
+        fb[i] = ((menu_img_buffer[i*3]/0xff)*0x1f)<<11 + ((menu_img_buffer[i*3+1]/0xff)*0x3f)<<5 + ((menu_img_buffer[i*3+2]/0xff)*0x1f);
+    }
+
+}
