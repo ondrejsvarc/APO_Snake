b7e9c946ad754651fcfaa5240a0686d2e1b8934b
Michal Komanec <michal.komanec@czechitas.cz>
2023-05-22 20:40:41 +0200

    Update main.c
    Added return 0; and recalculated image conversion constants

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
     
     fread(menu_img_buffer, sizeof(unsigned char), 320*480*3, menu_img_file);
     for (int i = 0; i < 320*480; i++) {
-        fb[i] = ((menu_img_buffer[i*3]/0xff)*0x1f)<<11 + ((menu_img_buffer[i*3+1]/0xff)*0x3f)<<5 + ((menu_img_buffer[i*3+2]/0xff)*0x1f);
+        fb[i] = ((menu_img_buffer[i*3]/0xff)*0x3f)<<11 + ((menu_img_buffer[i*3+1]/0xff)*0x7f)<<5 + ((menu_img_buffer[i*3+2]/0xff)*0x3f);
     }
 
     int c;
@@ -73,5 +73,5 @@ int main(int argc, char *argv[]) {
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
 
 
-
+    return 0;
 }
