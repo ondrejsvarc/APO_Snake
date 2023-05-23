ddfc8da37c95a1241dbd8ee59761ab28b07684c7
Michal Komanec <michal.komanec@czechitas.cz>
2023-05-23 18:11:22 +0200

    Moved drawing menu to draw_tools

--- draw_tools.h
+++ draw_tools.h
@@ -1,7 +1,9 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 
 
 void draw_pixel ( int x, int y, unsigned short color, unsigned short *fb );
 void drawTile ( int index, unsigned short color, unsigned short *fb );
 void drawFruit ( int index, unsigned short *fb );
 void drawMenuChoice ( int choice,unsigned short color, unsigned short *fb );
+void drawMenu(unsigned char *parlcd_mem_base, unsigned short *fb);
