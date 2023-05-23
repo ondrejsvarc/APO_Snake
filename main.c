c4c1e120fb6ca78f01782b82eb94b4f6d7c10de6
Michal Komanec <michal.komanec@czechitas.cz>
2023-05-23 17:48:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -30,9 +30,9 @@ int main(int argc, char *argv[]) {
     }
     parlcd_hx8357_init(parlcd_mem_base);
 
-    FILE *menu_img_file = fopen("menu_img.ppm", 'rb');
+    FILE *menu_img_file = fopen("/tmp/komanmi1/snake.ppm", "rb");
     if (menu_img_file == NULL) {
-        fprintf(stderr, "Error opening menu_img.ppm");
+        fprintf(stderr, "Error opening snake.ppm");
         exit(1);
     }
 
@@ -42,19 +42,21 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    fb = (unsigned short *)malloc(320*480*2);
+    fb = (unsigned short *)malloc(320*480*sizeof(unsigned short));
     if (fb == NULL) {
         fprintf(stderr, "Couldn't allocate memory for frame buffer!");
         exit(1);
     }
+    char *random_string = (char*)malloc(50);
     for (int i = 0; i < 4; i++) {
-        fscanf(menu_img_file, "%s\n");
+        fscanf(menu_img_file, "%s\n", random_string);
     }
     
     fread(menu_img_buffer, sizeof(unsigned char), 320*480*3, menu_img_file);
     for (int i = 0; i < 320*480; i++) {
         //fb[i] = (((((menu_img_buffer[i*3]/0xff)*0x3f)<<11)&0xf800) + ((((menu_img_buffer[i*3+1]/0xff)*0x7f)<<5)&0x7ff) + (((menu_img_buffer[i*3+2]/0xff)*0x3f)&0x1f));        
-        fb[i] = (((((menu_img_buffer[i*3]/0xff)*0x3f)>>1)<<12) + ((((menu_img_buffer[i*3+1]/0xff)*0x7f)>>1)<<6)) + (((menu_img_buffer[i*3+2]/0xff)*0x3f)>>1);    }
+        fb[i] = (((((menu_img_buffer[i*3]/0xff)*0x3f)>>1)<<12) + ((((menu_img_buffer[i*3+1]/0xff)*0x7f)>>1)<<6)) + (((menu_img_buffer[i*3+2]/0xff)*0x3f)>>1);        
+    }
 
     int c;
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
