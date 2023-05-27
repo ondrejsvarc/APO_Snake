0cd488086c8f331d0d9b0020b6085eef58debe2a
Ondřej Švarc <121796880+ondrejsvarc@users.noreply.github.com>
2023-05-27 11:48:43 +0200

    score tools doc

--- score_tools.c
+++ score_tools.c
@@ -1,11 +1,10 @@
-
 #include "score_tools.h"
-#include "draw_tools.h"
-#include "snake.h"
 
 void updateScores ( Snake *snake1, Snake *snake2, unsigned short *fb ) {
+    // Update scores
     snake1->score += snake1->length;
     snake2->score += snake2->length;
 
+    // Draw the scores
     draw_score( snake1->score, snake2->score, fb );
 }
--- score_tools.h
+++ score_tools.h
@@ -1,4 +1,18 @@
+/*******************************************************************
+  rgb_tools.c containing function for updating the score.
+ 
+  Created by Ondrej Svarc and Michal Komanec.
+ 
+ *******************************************************************/
 
+#include "draw_tools.h"
 #include "snake.h"
 
+/**
+ * Updates the scores of both snakes and draws them on the framebuffer.
+ *
+ * @param snake1 A pointer to the first snake.
+ * @param snake2 A pointer to the second snake.
+ * @param fb The framebuffer to draw the scores on.
+ */
 void updateScores ( Snake *snake1, Snake *snake2, unsigned short *fb );
